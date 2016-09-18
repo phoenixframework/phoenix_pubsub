06790a165d8d6a5cf38c053068d44572beebb7fc
Pawel Dawczak <pawel.dawczak@gmail.com>
2016-09-18 20:40:43 +0100

    Remove `from` from the `handle_update` arguments

--- lib/phoenix/tracker.ex
+++ lib/phoenix/tracker.ex
@@ -361,12 +361,12 @@ defmodule Phoenix.Tracker do
     {:reply, :ok, drop_presence(state, pid)}
   end
 
-  def handle_call({:update, pid, topic, key, meta_updater}, from, state) when is_function(meta_updater) do
-    handle_update({pid, topic, key, meta_updater}, from, state)
+  def handle_call({:update, pid, topic, key, meta_updater}, _from, state) when is_function(meta_updater) do
+    handle_update({pid, topic, key, meta_updater}, state)
   end
 
-  def handle_call({:update, pid, topic, key, new_meta}, from, state) do
-    handle_update({pid, topic, key, fn _ -> new_meta end}, from, state)
+  def handle_call({:update, pid, topic, key, new_meta}, _from, state) do
+    handle_update({pid, topic, key, fn _ -> new_meta end}, state)
   end
 
   def handle_call(:graceful_permdown, _from, state) do
@@ -607,7 +607,7 @@ defmodule Phoenix.Tracker do
     """
   end
 
-  defp handle_update({pid, topic, key, meta_updater}, _from, state) do
+  defp handle_update({pid, topic, key, meta_updater}, state) do
     case State.get_by_pid(state.presences, pid, topic, key) do
       nil ->
         {:reply, {:error, :nopresence}, state}
