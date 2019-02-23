49ec8315f2e52f63d1a6f9c8618f1d1877f31936
José Valim <jose.valim@plataformatec.com.br>
2019-02-23 19:07:37 +0100

    Release v1.1.2 with proper config

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,9 @@
 use Mix.Config
 
-import_config "#{Mix.env}.exs"
+config :phoenix_pubsub,
+  pubsub: [Phoenix.PubSub.Test.PubSub, [pool_size: 1]],
+  nodes: [:"node1@127.0.0.1", :"node2@127.0.0.1"]
+
+config :logger,
+  level: :info,
+  compile_time_purge_level: :info
